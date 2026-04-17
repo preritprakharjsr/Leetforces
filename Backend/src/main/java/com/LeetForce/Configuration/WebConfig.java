0cf7beaecfdd4ec849be8d1c3d436ea102feac41
easy_friend <preritprakhar.jsr@gmail.com>
2026-04-18 01:58:55 +0530

    Leetcode fetch service

---
+++ Backend/src/main/java/com/LeetForce/Configuration/WebConfig.java
@@ -0,0 +1,15 @@
+package com.LeetForce.Configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebClient leetcodeClient(){
+        return WebClient.builder()
+                .baseUrl("https://leetcode.com")
+                .build();
+    }
+}
